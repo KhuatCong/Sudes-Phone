c22c0667f895b9a86fae1c8192097aecbd132420
KhuatCong <khuatphucong06092002@gmail.com>
2023-11-30 00:53:05 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.slide-category').slick({
+        dots: false,
+        arrows:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 8,
+        slidesToScroll:1,
+    });
+})
