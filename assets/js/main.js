aed956bc06f37ff1f9a1a6fdca493b42064e2c2b
KhuatCong <khuatphucong06092002@gmail.com>
2023-11-30 20:03:27 +0700

    hihihi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
 $(function () {
-    $('.slide-category').slick({
-        dots: false,
-        arrows:false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 8,
-        slidesToScroll:1,
-    });
+    // $('.slide-category').slick({
+    //     dots: false,
+    //     arrows:false,
+    //     infinite: false,
+    //     speed: 300,
+    //     slidesToShow: 8,
+    //     slidesToScroll:1,
+    // });
+
+    $('.bar-mobile').click(function(){
+        $('.box-menu').toggleClass('current');
+        $('.opacity').css('display','block');
+    })
+    $('.close-box').click(function(){
+        $('.box-menu').toggleClass('current');
+        $('.opacity').css('display','none');
+    })
+    $('.opacity').click(function(){
+        $('.box-menu').toggleClass('current');
+        $(this).css('display','none');
+    })
 })
